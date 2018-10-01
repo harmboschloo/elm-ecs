e059db196b23b963d459c3f5f4346c905c210d88
Harm Boschloo <harm@boschloo.net>
2018-10-01 21:32:24 +0200

    added styling

--- ui/Main.elm
+++ ui/Main.elm
@@ -5,6 +5,7 @@ import Browser
 import Browser.Dom as Dom
 import Browser.Navigation as Navigation
 import Css
+import Css.Global
 import EcsGenerator
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes as Attributes
@@ -66,7 +67,16 @@ update msg model =
             ( model, Cmd.none )
 
         LinkClicked urlRequest ->
-            ( model, Cmd.none )
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model
+                    , Navigation.pushUrl model.navigationKey (Url.toString url)
+                    )
+
+                Browser.External url ->
+                    ( model
+                    , Navigation.load url
+                    )
 
         UrlChanged url ->
             let
@@ -152,66 +162,163 @@ view model =
     { title = "ECS Generator"
     , body =
         List.map Html.toUnstyled
-            [ viewEcsModuleNameInput model.ecsModuleName
-            , Html.hr [] []
-            , viewComponentInputs model.componentModuleName model.componentTypeName
-            , Lazy.lazy viewComponents model.components
-            , Html.hr [] []
+            [ Css.Global.global
+                [ Css.Global.everything
+                    [ Css.boxSizing Css.borderBox
+                    , Css.fontFamily Css.monospace
+                    , Css.fontSize (Css.px 16)
+                    ]
+                , Css.Global.body
+                    [ Css.padding Css.zero
+                    , Css.margin Css.zero
+                    ]
+                ]
+            , viewHeading
+            , Html.div
+                [ Attributes.css
+                    [ Css.displayFlex
+                    , Css.justifyContent Css.center
+                    ]
+                ]
+                [ Html.div
+                    [ Attributes.css
+                        [ Css.flexDirection Css.column
+                        ]
+                    ]
+                    [ viewEcsModuleNameInput model.ecsModuleName
+                    , viewComponents
+                        model.componentModuleName
+                        model.componentTypeName
+                        model.components
+                    ]
+                ]
+            , viewHeading2 "generated code"
             , Lazy.lazy2 viewGeneratResult model.ecsModuleName model.components
             ]
     }
 
 
+viewHeading : Html Msg
+viewHeading =
+    Html.h1
+        [ Attributes.css
+            [ Css.fontSize (Css.em 1)
+            , Css.textAlign Css.center
+            , Css.margin3 (Css.px 5) Css.zero Css.zero
+            ]
+        ]
+        (List.intersperse (Html.text " - ")
+            [ Html.text "ecs generator"
+            , Html.a
+                [ Attributes.href "https://harmboschloo.github.io/elm-ecs-generator/#Ecs%3BComponents%2CAi%3BComponents%2CCollectable%3BComponents%2CCollector%3BComponents%2CDestroy%3BComponents%2CKeyControlsMap%3BComponents%2CMotion%3BComponents%2CPosition%3BComponents%2CScale%3BComponents%2CScaleAnimation%3BComponents%2CSprite%3BComponents%2CVelocity%3BComponents.Controls%2CControls" ]
+                [ Html.text "example" ]
+            , Html.a
+                [ Attributes.href "https://harmboschloo.github.io/elm-ecs-generator/example/build/" ]
+                [ Html.text "example demo" ]
+            , Html.a
+                [ Attributes.href "https://github.com/harmboschloo/elm-ecs-generator" ]
+                [ Html.text "code" ]
+            ]
+        )
+
+
+viewHeading2 : String -> Html Msg
+viewHeading2 heading =
+    Html.h2
+        [ Attributes.css
+            [ Css.fontSize (Css.em 0.9)
+            , Css.textAlign Css.center
+            , Css.margin3 (Css.px 8) Css.zero (Css.px 4)
+            ]
+        ]
+        [ Html.text heading ]
+
+
 viewEcsModuleNameInput : String -> Html Msg
 viewEcsModuleNameInput moduleName =
     Html.div []
-        [ Html.text "ecs module name: "
+        [ viewHeading2 "ecs module name"
         , Html.input
             [ Attributes.value moduleName
             , Attributes.required True
             , Events.onInput EcsModuleNameChanged
+            , Attributes.css
+                [ Css.width (Css.pct 100)
+                , Css.padding (Css.px 4)
+                ]
             ]
             []
         ]
 
 
-viewComponentInputs : String -> String -> Html Msg
-viewComponentInputs moduleName typeName =
-    Html.form [ Events.onSubmit ComponentSubmitted ]
-        [ Html.text "component: "
-        , Html.input
-            [ Attributes.id "componentModuleNameInput"
-            , Attributes.value moduleName
-            , Attributes.required True
-            , Attributes.placeholder "module name"
-            , Attributes.autofocus True
-            , Events.onInput ComponentModuleNameChanged
-            ]
-            []
-        , Html.input
-            [ Attributes.value typeName
-            , Attributes.required True
-            , Attributes.placeholder "type name"
-            , Events.onInput ComponentTypeNameChanged
-            ]
-            []
-        , Html.button [] [ Html.text "add" ]
-        ]
-
-
-viewComponents : Set ( String, String ) -> Html Msg
-viewComponents components =
-    Html.div [] (components |> Set.toList |> List.sort |> List.map viewComponent)
-
-
-viewComponent : ( String, String ) -> Html Msg
-viewComponent ( moduleName, typeName ) =
+viewComponents : String -> String -> Set ( String, String ) -> Html Msg
+viewComponents moduleNameInput typeNameInput components =
     Html.div []
-        [ Html.text (moduleName ++ "." ++ typeName)
-        , Html.button
-            [ Events.onClick (ComponentRemoved ( moduleName, typeName )) ]
-            [ Html.text "remove" ]
+        [ viewHeading2 "components"
+        , Html.form
+            [ Events.onSubmit ComponentSubmitted
+            , Attributes.css
+                [ Css.property "display" "grid"
+                , Css.property "grid-template-columns" "auto auto auto"
+                ]
+            ]
+            (viewComponentInputs moduleNameInput typeNameInput
+                :: (components |> Set.toList |> List.sort |> List.map viewComponent)
+                |> List.concat
+            )
         ]
+
+
+viewComponentInputs : String -> String -> List (Html Msg)
+viewComponentInputs moduleName typeName =
+    [ Html.input
+        [ Attributes.id "componentModuleNameInput"
+        , Attributes.value moduleName
+        , Attributes.required True
+        , Attributes.placeholder "module name"
+        , Attributes.autofocus True
+        , Events.onInput ComponentModuleNameChanged
+        , Attributes.css
+            [ Css.padding (Css.px 4)
+            ]
+        ]
+        []
+    , Html.input
+        [ Attributes.value typeName
+        , Attributes.required True
+        , Attributes.placeholder "type name"
+        , Events.onInput ComponentTypeNameChanged
+        , Attributes.css
+            [ Css.padding (Css.px 4)
+            ]
+        ]
+        []
+    , Html.button
+        [ Attributes.type_ "submit" ]
+        [ Html.text "add" ]
+    ]
+
+
+viewComponent : ( String, String ) -> List (Html Msg)
+viewComponent ( moduleName, typeName ) =
+    [ Html.span
+        [ Attributes.css
+            [ Css.padding (Css.px 5)
+            ]
+        ]
+        [ Html.text moduleName ]
+    , Html.span
+        [ Attributes.css
+            [ Css.padding (Css.px 5)
+            ]
+        ]
+        [ Html.text typeName ]
+    , Html.button
+        [ Events.onClick (ComponentRemoved ( moduleName, typeName ))
+        , Attributes.type_ "button"
+        ]
+        [ Html.text "remove" ]
+    ]
 
 
 viewGeneratResult : String -> Set ( String, String ) -> Html Msg
@@ -263,7 +370,10 @@ textAreaAttributes numberOfLines =
     [ Attributes.readonly True
     , Attributes.rows (numberOfLines + 1)
     , Attributes.wrap "off"
-    , Attributes.css [ Css.width (Css.pct 100) ]
+    , Attributes.css
+        [ Css.width (Css.pct 100)
+        , Css.fontSize (Css.px 14)
+        ]
     ]
 
 
